b708a9486d589cb43d9d96f7d9041cdd936d0b60
Croppedeyebrow <lee940706@gmail.com>
2024-05-03 15:03:38 +0900

    0503. 1

--- lib/daily_screen.dart
+++ lib/daily_screen.dart
@@ -35,6 +35,67 @@ class _DailyScreenState extends State<DailyScreen> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [Icon(Icons.bar_chart), Icon(Icons.more_vert)],
+                  ),
+                  SizedBox(height: 20),
+                  Column(
+                    children: [
+                      Container(
+                        height: 88,
+                        width: 88,
+                        decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: AssetImage("assets/images/profile.jpg"),
+                                fit: BoxFit.cover)),
+                      ),
+                      SizedBox(height: 20),
+                      Container(
+                          width: (size.width - 40) * 0.6,
+                          child: Column(
+                            children: [
+                              Text(
+                                "코딩지휘소",
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: mainFontColor),
+                              ),
+                              SizedBox(height: 20),
+                              Text(
+                                " 안드로이드 개발자",
+                                style: TextStyle(
+                                    fontSize: 12,
+                                    fontWeight: FontWeight.w500,
+                                    color: black),
+                              ),
+                            ],
+                          ))
+                    ],
+                  ),
+                  SizedBox(height: 54),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "\₩400000000 ",
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                color: mainFontColor),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            "Total Revenue ",
+                            style: TextStyle(
+                                fontSize: 12,
+                                fontWeight: FontWeight.w600,
+                                color: black),
+                          )
+                        ],
+                      )
+                    ],
                   )
                 ],
               ),
